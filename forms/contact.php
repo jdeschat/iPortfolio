e371700d9920001b2dc224aca2d79802047fc3f9
Joelle DesChatelets <jdeschat@gmail.com>
2021-09-08 14:21:01 -0400

    portfolio details

--- forms/contact.php
+++ forms/contact.php
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  $contact->smtp = array(
-    'host' => 'jdeschat@gmail.com',
-    'username' => 'jdeschat',
-    'password' => 'pass',
-    'port' => '587'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'jdeschat@gmail.com',
+  //   'username' => 'jdeschat',
+  //   'password' => '',
+  //   'port' => '587'
+  // );
   
 
   $contact->add_message( $_POST['name'], 'From');
